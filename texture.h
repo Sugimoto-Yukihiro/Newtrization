d6b4a3f9961e01a33d19a04c47528923c00e70c3
Yukihiro <yukihiro.sugimoto0217@gmail.com>
2021-07-03 16:27:16 +0900

    テクスチャクラスの仮作成

--- texture.h
+++ texture.h
@@ -9,6 +9,22 @@
 //*****************************************************************************
 
 //*****************************************************************************
+// クラス定義
+//*****************************************************************************
+class CTexture
+{
+public:
+	void SetV(float V);
+	void SetU(float U);
+	void DrawTexture(char* TextureName, D3DXVECTOR2 Position, D3DXCOLOR Color, float Rotation);
+
+private:
+	//------------------- メンバ変数
+	D3DXVECTOR2		m_vTexPos;		// テクスチャの描画位置
+	float			m_fU, m_fV;		// UV座標系
+};
+
+//*****************************************************************************
 // プロトタイプ宣言
 //*****************************************************************************
 void SetVertex(ID3D11Buffer *buf, float X, float Y, float Width, float Height, float U, float V, float UW, float VH);
